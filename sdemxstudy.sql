20513a6d36451a1b851428a452eadcbe86cd7b9e
ldthang2201 <ldthang2201@gmail.com>
2021-01-23 10:20:46 +0700

    Update Database (23/01/2021) ep1

--- sdemxstudy.sql
+++ sdemxstudy.sql
@@ -11,7 +11,7 @@
  Target Server Version : 100414
  File Encoding         : 65001
 
- Date: 22/01/2021 21:24:54
+ Date: 23/01/2021 10:20:06
 */
 
 SET NAMES utf8mb4;
@@ -120,7 +120,7 @@ CREATE TABLE `course`  (
 -- Records of course
 -- ----------------------------
 INSERT INTO `course` VALUES (1, 'Introducing ID Hardware 1', 1, 2, 'this is tinyDes', 1500000, 1.00, 'this is full Des', '2021-01-16', '2021-01-16', 645716, b'1', b'0', 'English');
-INSERT INTO `course` VALUES (2, 'PLC Programming From Scratch (PLC I)', 1, 2, 'This course will give a person with no prior experience the basic tools necessary to create a PLC program from scratch.', 1000000, 0.90, '<div><p>Interested in the field of Machine Learning?&nbsp;Then this course is for you!</p>\r\n    <p>This course has been designed by two professional Data Scientists so that we can share our knowledge and help you learn complex theory,&nbsp;algorithms, and coding libraries in a simple way.</p>\r\n    <p>We will walk you step-by-step into the World of Machine Learning. With every tutorial, you will develop new skills and improve your understanding of this challenging yet lucrative sub-field of Data Science.</p>\r\n    <p>This course is&nbsp;fun and exciting, but at the same time, we dive deep into Machine Learning. It is structured the following way:</p>\r\n    <ul><li><p>Part 1 - Data Preprocessing</p> </li> <li><p>Part 2 - Regression: Simple Linear Regression, Multiple Linear Regression,&nbsp;Polynomial&nbsp;Regression,&nbsp;SVR, Decision Tree Regression,&nbsp;Random Forest Regression</p></li></ul><p>Moreover, the course is packed with practical exercises that are based on real-life&nbsp;examples. So not only will you learn the theory, but you will also get some hands-on practice building your own models.</p><p>And as a bonus, this course includes both&nbsp;Python and R&nbsp;code templates which you can download and use on your own projects.</p></div>', '2021-01-16', '2021-01-16', 544886, b'1', b'0', 'English');
+INSERT INTO `course` VALUES (2, 'PLC Programming From Scratch (PLC I)', 1, 2, 'This course will give a person with no prior experience the basic tools necessary to create a PLC program from scratch.', 1000000, 0.90, '<div><p>Interested in the field of Machine Learning?&nbsp;Then this course is for you!</p>\r\n    <p>This course has been designed by two professional Data Scientists so that we can share our knowledge and help you learn complex theory,&nbsp;algorithms, and coding libraries in a simple way.</p>\r\n    <p>We will walk you step-by-step into the World of Machine Learning. With every tutorial, you will develop new skills and improve your understanding of this challenging yet lucrative sub-field of Data Science.</p>\r\n    <p>This course is&nbsp;fun and exciting, but at the same time, we dive deep into Machine Learning. It is structured the following way:</p>\r\n    <ul><li><p>Part 1 - Data Preprocessing</p> </li> <li><p>Part 2 - Regression: Simple Linear Regression, Multiple Linear Regression,&nbsp;Polynomial&nbsp;Regression,&nbsp;SVR, Decision Tree Regression,&nbsp;Random Forest Regression</p></li></ul><p>Moreover, the course is packed with practical exercises that are based on real-life&nbsp;examples. So not only will you learn the theory, but you will also get some hands-on practice building your own models.</p><p>And as a bonus, this course includes both&nbsp;Python and R&nbsp;code templates which you can download and use on your own projects.</p></div>', '2021-01-16', '2021-01-16', 544887, b'1', b'0', 'English');
 INSERT INTO `course` VALUES (3, 'Practical PLC Programming (PLC II)', 1, 2, 'This course will give an intermediate-level student the experience necessary to originate complex, logical solutions.', 1000000, 1.00, '<div><p>Interested in the field of Machine Learning?&nbsp;Then this course is for you!</p>\r\n    <p>This course has been designed by two professional Data Scientists so that we can share our knowledge and help you learn complex theory,&nbsp;algorithms, and coding libraries in a simple way.</p>\r\n    <p>We will walk you step-by-step into the World of Machine Learning. With every tutorial, you will develop new skills and improve your understanding of this challenging yet lucrative sub-field of Data Science.</p>\r\n    <p>This course is&nbsp;fun and exciting, but at the same time, we dive deep into Machine Learning. It is structured the following way:</p>\r\n    <ul><li><p>Part 1 - Data Preprocessing</p> </li> <li><p>Part 2 - Regression: Simple Linear Regression, Multiple Linear Regression,&nbsp;Polynomial&nbsp;Regression,&nbsp;SVR, Decision Tree Regression,&nbsp;Random Forest Regression</p></li></ul><p>Moreover, the course is packed with practical exercises that are based on real-life&nbsp;examples. So not only will you learn the theory, but you will also get some hands-on practice building your own models.</p><p>And as a bonus, this course includes both&nbsp;Python and R&nbsp;code templates which you can download and use on your own projects.</p></div>', '2021-01-16', '2021-01-16', 76794, b'1', b'0', 'English');
 INSERT INTO `course` VALUES (4, 'Introducing ID Hardware 2 ', 1, 2, NULL, 1500000, 1.00, '<div><p>Interested in the field of Machine Learning?&nbsp;Then this course is for you!</p>\r\n    <p>This course has been designed by two professional Data Scientists so that we can share our knowledge and help you learn complex theory,&nbsp;algorithms, and coding libraries in a simple way.</p>\r\n    <p>We will walk you step-by-step into the World of Machine Learning. With every tutorial, you will develop new skills and improve your understanding of this challenging yet lucrative sub-field of Data Science.</p>\r\n    <p>This course is&nbsp;fun and exciting, but at the same time, we dive deep into Machine Learning. It is structured the following way:</p>\r\n    <ul><li><p>Part 1 - Data Preprocessing</p> </li> <li><p>Part 2 - Regression: Simple Linear Regression, Multiple Linear Regression,&nbsp;Polynomial&nbsp;Regression,&nbsp;SVR, Decision Tree Regression,&nbsp;Random Forest Regression</p></li></ul><p>Moreover, the course is packed with practical exercises that are based on real-life&nbsp;examples. So not only will you learn the theory, but you will also get some hands-on practice building your own models.</p><p>And as a bonus, this course includes both&nbsp;Python and R&nbsp;code templates which you can download and use on your own projects.</p></div>', '2021-01-16', '2021-01-16', 597076, b'1', b'0', 'English');
 INSERT INTO `course` VALUES (5, 'Learn Ethical Hacking From Scratch', 2, 2, 'Become an ethical hacker that can hack computer systems like black hat hackers and secure them like security experts.', 2000000, 1.00, '<div><p>Interested in the field of Machine Learning?&nbsp;Then this course is for you!</p>\r\n    <p>This course has been designed by two professional Data Scientists so that we can share our knowledge and help you learn complex theory,&nbsp;algorithms, and coding libraries in a simple way.</p>\r\n    <p>We will walk you step-by-step into the World of Machine Learning. With every tutorial, you will develop new skills and improve your understanding of this challenging yet lucrative sub-field of Data Science.</p>\r\n    <p>This course is&nbsp;fun and exciting, but at the same time, we dive deep into Machine Learning. It is structured the following way:</p>\r\n    <ul><li><p>Part 1 - Data Preprocessing</p> </li> <li><p>Part 2 - Regression: Simple Linear Regression, Multiple Linear Regression,&nbsp;Polynomial&nbsp;Regression,&nbsp;SVR, Decision Tree Regression,&nbsp;Random Forest Regression</p></li></ul><p>Moreover, the course is packed with practical exercises that are based on real-life&nbsp;examples. So not only will you learn the theory, but you will also get some hands-on practice building your own models.</p><p>And as a bonus, this course includes both&nbsp;Python and R&nbsp;code templates which you can download and use on your own projects.</p></div>', '2021-01-17', '2021-01-17', 596873, b'1', b'0', 'English');
@@ -130,7 +130,7 @@ INSERT INTO `course` VALUES (8, 'The Complete Cyber Security Course : Network Se
 INSERT INTO `course` VALUES (9, 'Understanding Macroeconomics for University and Business', 3, 2, 'Explaining the economics of fiscal policy, money creation, and monetary policy', 800000, 0.90, NULL, '2021-01-17', '2021-01-17', 850292, b'1', b'0', 'English');
 INSERT INTO `course` VALUES (10, 'The Circular Economy', 3, 2, 'What and Where Are the Opportunities for Entrepreneurs?', 1200000, 1.00, NULL, '2021-01-17', '2021-01-17', 223419, b'1', b'0', 'English');
 INSERT INTO `course` VALUES (11, 'The Complete Investment Banking Course 2021', 4, 2, 'The #1 Course to Land a Job in Investment Banking. IPOs, Bonds, M&A, Trading, LBOs, Valuation: Everything is included!', 1050000, 1.00, NULL, '2021-01-17', '2021-01-17', 75391, b'1', b'0', 'English');
-INSERT INTO `course` VALUES (12, 'Bookkeeping Basics #1: Understand the Fundamentals', 5, 2, 'Learn bookkeeping terms and concepts to make owners, employees & students more confident and successful!', 900000, 1.00, NULL, '2021-01-17', '2021-01-17', 112, b'1', b'0', 'English');
+INSERT INTO `course` VALUES (12, 'Bookkeeping Basics #1: Understand the Fundamentals', 5, 2, 'Learn bookkeeping terms and concepts to make owners, employees & students more confident and successful!', 900000, 1.00, NULL, '2021-01-17', '2021-01-17', 113, b'1', b'0', 'English');
 INSERT INTO `course` VALUES (13, 'Accounting: From Beginner to Advanced!', 5, 2, 'Learn accounting like never before. Learn easy and fast. Easy to understand Accounting. #1 accounting course online.', 2100000, 0.95, NULL, '2021-01-19', '2021-01-17', 22096, b'1', b'0', 'English');
 INSERT INTO `course` VALUES (14, 'Pixel art for Video games', 6, 2, 'The essential course for creating stunning pixel art graphics for video-games or stylish illustrations and designs', 650000, 1.00, NULL, '2021-01-17', '2021-01-17', 1654, b'1', b'0', 'English');
 INSERT INTO `course` VALUES (15, 'Blender 2.8 Game Character Creation', 6, 2, 'Learn to Create Game Characters with Blender 2.8 and Unity', 500000, 0.85, '', '2021-01-17', '2021-01-17', 22054, b'1', b'0', 'English');
@@ -199,7 +199,7 @@ CREATE TABLE `user`  (
 -- Records of user
 -- ----------------------------
 INSERT INTO `user` VALUES (0, 'sdsd', 'dsd', 'sdsd', 'dsd', '2021-01-18', NULL, 3, 0);
-INSERT INTO `user` VALUES (1, 'admin', 'admin', 'Administrator', NULL, NULL, NULL, 1, 0);
+INSERT INTO `user` VALUES (1, 'admin', 'newpassword', 'Administrator', NULL, NULL, NULL, 1, 0);
 INSERT INTO `user` VALUES (2, 'ldthang', 'ksldajlksjdilkjalsfd', 'Le Duc Thang', 'ldthang2201@gmail.com', '2021-01-16', NULL, 2, 0);
 INSERT INTO `user` VALUES (3, 'tnthanh', 'trannhatthanh', 'Tran Nhat Thanh', 'thanh@gmail.com', '2021-01-17', NULL, 3, 0);
 INSERT INTO `user` VALUES (4, 'hhelomoinguoi', 'leducthangsad', 'Le Duc Thang', 'ldthang@email', '2000-01-01', NULL, 3, 0);
@@ -233,6 +233,11 @@ INSERT INTO `watchlist` VALUES (4, 2, '2021-01-20');
 INSERT INTO `watchlist` VALUES (4, 3, '2021-01-20');
 INSERT INTO `watchlist` VALUES (4, 12, '2021-01-20');
 INSERT INTO `watchlist` VALUES (4, 31, '2021-01-06');
+INSERT INTO `watchlist` VALUES (134, 2, '2021-01-23');
+INSERT INTO `watchlist` VALUES (134, 5, '2021-01-23');
+INSERT INTO `watchlist` VALUES (134, 8, '2021-01-23');
+INSERT INTO `watchlist` VALUES (134, 10, '2021-01-23');
+INSERT INTO `watchlist` VALUES (134, 12, '2021-01-23');
 
 -- ----------------------------
 -- Table structure for wishlist
@@ -247,6 +252,18 @@ CREATE TABLE `wishlist`  (
   CONSTRAINT `FK_wishlist_course` FOREIGN KEY (`courID`) REFERENCES `course` (`courID`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   CONSTRAINT `FK_wishlist_user` FOREIGN KEY (`userID`) REFERENCES `user` (`userID`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of wishlist
+-- ----------------------------
+INSERT INTO `wishlist` VALUES (4, 2, '2021-01-23');
+INSERT INTO `wishlist` VALUES (8, 15, '2021-01-23');
+INSERT INTO `wishlist` VALUES (134, 1, '2021-01-23');
+INSERT INTO `wishlist` VALUES (134, 2, '2021-01-23');
+INSERT INTO `wishlist` VALUES (134, 5, '2021-01-23');
+INSERT INTO `wishlist` VALUES (134, 9, '2021-01-23');
+INSERT INTO `wishlist` VALUES (134, 25, '2021-01-23');
+INSERT INTO `wishlist` VALUES (134, 30, '2021-01-23');
 
 -- ----------------------------
 -- View structure for top4categoriesmostsubc
@@ -277,6 +294,39 @@ from branch, category, course LEFT JOIN feedback on course.courID=feedback.courI
 where branch.branchID=category.branchID and category.catID=course.catID and (DATEDIFF(NOW(),dateUpload)<=7)
 GROUP BY course.courID, title, branch.branchID, teacherID, prices, sale, dateUpload, views, premium
 ORDER BY dateUpload DESC LIMIT 10 ;
+
+-- ----------------------------
+-- Procedure structure for add_removeWishList
+-- ----------------------------
+DROP PROCEDURE IF EXISTS `add_removeWishList`;
+delimiter ;;
+CREATE PROCEDURE `add_removeWishList`(IN uID int(11),coID int(11))
+begin
+	IF (EXISTS (select * from wishlist where userID=uID and courID=coID)) THEN
+	Delete from wishlist where userID=uID and courID=coID;
+ELSE
+	INSERT into wishlist values(uID,coID,now());
+END IF;
+end
+;;
+delimiter ;
+
+-- ----------------------------
+-- Procedure structure for add_WatchList
+-- ----------------------------
+DROP PROCEDURE IF EXISTS `add_WatchList`;
+delimiter ;;
+CREATE PROCEDURE `add_WatchList`(IN uID int(11),coID int(11))
+begin
+	IF (EXISTS (select * from watchlist where userID=uID and courID=coID)) THEN
+	UPDATE course set views=views+1 where courID=coID;
+ELSE
+	INSERT into watchlist values(uID,coID,now());
+	UPDATE course set views=views+1 where courID=coID;
+END IF;
+end
+;;
+delimiter ;
 
 -- ----------------------------
 -- Function structure for CountNoCourseByBranchID
@@ -332,6 +382,24 @@ BEGIN
 	RETURN (select avg(rate)
 					from course LEFT JOIN feedback on course.courID=feedback.courID
 					where course.courID=id);
+END
+;;
+delimiter ;
+
+-- ----------------------------
+-- Function structure for f_checkWishList
+-- ----------------------------
+DROP FUNCTION IF EXISTS `f_checkWishList`;
+delimiter ;;
+CREATE FUNCTION `f_checkWishList`(`uID` int,`coID` int)
+ RETURNS bit(1)
+BEGIN
+	IF (EXISTS (select * from wishlist where userID=uID and courID=coID)) THEN
+	return 1;
+ELSE
+	return 0;
+END IF;
+
 END
 ;;
 delimiter ;
@@ -429,11 +497,11 @@ delimiter ;
 -- ----------------------------
 DROP PROCEDURE IF EXISTS `sp_ChangePassword`;
 delimiter ;;
-CREATE PROCEDURE `sp_ChangePassword`(IN uID int(11),newPass varchar(255))
+CREATE PROCEDURE `sp_ChangePassword`(IN uName varchar(30),newPass varchar(255))
 begin
 update `user`
 set `password` = newPass
-where userID=uID;
+where userName=uName;
 end
 ;;
 delimiter ;
@@ -465,6 +533,34 @@ begin
 	from feedback
 	where courID=id;
 end
+;;
+delimiter ;
+
+-- ----------------------------
+-- Procedure structure for sp_getMyWatchListByUserID
+-- ----------------------------
+DROP PROCEDURE IF EXISTS `sp_getMyWatchListByUserID`;
+delimiter ;;
+CREATE PROCEDURE `sp_getMyWatchListByUserID`(IN uID int(11))
+BEGIN 
+	select course.courID, title,catID,teacherID,dateUpload,lastUpdate,premium
+	from course, watchlist
+	where watchlist.userID=uID and course.courID=watchlist.courID;
+END
+;;
+delimiter ;
+
+-- ----------------------------
+-- Procedure structure for sp_getMyWishListByUserID
+-- ----------------------------
+DROP PROCEDURE IF EXISTS `sp_getMyWishListByUserID`;
+delimiter ;;
+CREATE PROCEDURE `sp_getMyWishListByUserID`(IN uID int(11))
+BEGIN 
+	select course.courID, title,catID,teacherID,dateUpload,lastUpdate,premium
+	from course, wishlist
+	where wishlist.userID=uID and course.courID=wishlist.courID;
+END
 ;;
 delimiter ;
 
